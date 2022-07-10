e2069b4b6c9aee9574bef6ca75ee5967e98473a1
Loktysh Alexandr <vebster24@mail.ru>
2022-07-10 21:54:52 +0300

    feat: add basic server

---
+++ src/user/userResolver.js
@@ -0,0 +1,18 @@
+import userService from './userService';
+
+export const userResolvers = {
+  Query: {
+    user: async (parent, { id }, { dataSources }) => {
+      return UsersAPI.getUser(id);
+    },
+    jwt: (parent, { authorizeUserInput }, { dataSources }) => {
+      return UsersAPI.loginUser(authorizeUserInput);
+    },
+  },
+  Mutation: {
+    register: (parent, { registerUserInput }, { dataSources }) => {
+      return UsersAPI.registerUser(registerUserInput);
+    },
+  },
+  User: { id: (parent) => parent._id },
+};
